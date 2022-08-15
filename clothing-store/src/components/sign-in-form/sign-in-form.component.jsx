616c7355d2d6df21d2f620ee74f4e9f19569b19c
Charles Willcockson <willcockson.charles@gmail.com>
2022-08-15 17:29:41 -0500

    finalizing auth listener

--- clothing-store/src/components/sign-in-form/sign-in-form.component.jsx
+++ clothing-store/src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,9 +1,8 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import { signInWithGooglePopup, createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input-component";
 import './sign-in-form.styles.scss';
 import Button from "../button/button.component";
-import { UserContext } from "../../contexts/user.context";
 
 const defaultFormFields = {
     email: '',
@@ -13,17 +12,12 @@ const defaultFormFields = {
 const SignInForm = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { email, password } = formFields;
-
-    const { setCurrentUser } = useContext(UserContext);
-
     
-
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
     const signInWithGoogle = async () => {
         const { user } = await signInWithGooglePopup();
-        setCurrentUser(user);
         createUserDocumentFromAuth(user);
     };
 
@@ -32,8 +26,6 @@ const SignInForm = () => {
 
         try {
             const { user } = await signInAuthUserWithEmailAndPassword(email, password);
-            setCurrentUser(user);
-
             resetFormFields();
         } catch (error) {
             switch (error.code) {
