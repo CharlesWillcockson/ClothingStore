c673eab89dbaf5997ea46a8f313069ffa4468973
Charles Willcockson <willcockson.charles@gmail.com>
2022-08-15 10:34:30 -0500

    added signOutUser to firebase utils

--- clothing-store/src/utils/firebase/firebase.utils.js
+++ clothing-store/src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut} from 'firebase/auth';
 import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore'
  
 // Your web app's Firebase configuration
@@ -64,5 +64,7 @@ export const createAuthUserWithEmailAndPassword = async (email, password) => {
   
     return await signInWithEmailAndPassword(auth, email, password);
   };
+
+  export const signOutUser = async () => await signOut(auth);
   
   
