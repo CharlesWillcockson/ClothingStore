0c94d96cc2927c86a04a208736120cc7514da77b
Charles Willcockson <willcockson.charles@gmail.com>
2022-08-22 10:02:23 -0500

    connected shap-data.js to firebase db

--- clothing-store/src/components/routes/shop/shop.component.jsx
+++ clothing-store/src/components/routes/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 
-import { ProductsContext } from "../../../contexts/products.contect";
+import { ProductsContext } from "../../../contexts/products.context";
 import ProductCard from '../../product-card/product-card.component';
 import './shop.styles.scss';
 
